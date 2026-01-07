3fe66d567f10a2abe53661d47b5b9e421f4cec57
9TMP6 <dattmpnguyen96@gmail.com>
2026-01-07 16:19:09 +0700

    Delete buoi2/bai3.java

--- buoi2/bai3.java
+++
@@ -1,9 +0,0 @@
-package buoi2;
-
-public class bai3 {
-    public static void main(String[] args) {
-        for (int i = 2; i <= 11; i += 2) {
-            System.out.println(i);
-        }
-    }
-}
