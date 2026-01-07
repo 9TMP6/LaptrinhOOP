de31408bc60bb73432f25ee90a362506e2203c81
9TMP6 <dattmpnguyen96@gmail.com>
2026-01-07 15:44:24 +0700

    Add files via upload

---
+++ buoi2/bai1.java
@@ -0,0 +1,13 @@
+package buoi2;
+
+import java.util.Scanner;
+
+public class bai1 {
+    public static void main(String[] args) {
+        int i = 1;
+        while (i <= 5) {
+            System.out.println("Hello world!");
+            i++;
+        }
+    }
+}
---
+++ buoi2/bai3.java
@@ -0,0 +1,9 @@
+package buoi2;
+
+public class bai3 {
+    public static void main(String[] args) {
+        for (int i = 2; i <= 11; i += 2) {
+            System.out.println(i);
+        }
+    }
+}
